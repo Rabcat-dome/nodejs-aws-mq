5218a2a4e2cf45bccfe8a93dd460bc51b69046fa
VISARUTP <visarut.p@pttdigital.com>
2023-03-17 00:45:56 +0700

    no message

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # nodejs-aws-mq
+https://www.codementor.io/@antonlawrence/building-a-simple-messaging-system-using-activemq-1et7m9nh5a
+https://github.com/aws-samples/aws-sar-lambda-publish-amazonmq
---
+++ test-publisher.js
@@ -0,0 +1,40 @@
+/*
+  Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this
+  software and associated documentation files (the "Software"), to deal in the Software
+  without restriction, including without limitation the rights to use, copy, modify,
+  merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+  permit persons to whom the Software is furnished to do so.
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+  INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+  PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+const mqtt = require('mqtt')
+const uuidv1 = require('uuid/v1');
+
+// Get arguments from the command line
+// node app.js "username" "password" "wss://endpoint:port" "some/topic"
+//const args = process.argv.slice(2)
+
+let options = {
+  username: "activemq",
+  password: "874521Active",
+  clientId: 'mqttLambda_' + uuidv1()
+}
+
+let mqEndpoint = "wss://b-769365f5-edc6-47cf-a2d3-26a29cea937b-1.mq.ap-southeast-1.amazonaws.com:61619"
+let topic = "/queue/testTopic"
+
+let client = mqtt.connect( mqEndpoint, options)
+
+// Once connected subscribe to the topic
+
+setInterval(() => {
+    let num = Math.random();
+    console.log(num);
+    client.publish(topic, "hello from NodeJS =>" + num);
+}, 5000);
